83827e623dcf17ea9dc9d30cf9f40e931c91fe51
awexli <awexli@mail.sfsu.edu>
2020-01-14 19:09:18 -0800

    initial

---
+++ library.js
@@ -0,0 +1,16 @@
+let myLibrary = [];
+
+function Book(title, author, status) {
+    // TODO
+    this.title = title;
+    this.author = author;
+    this.status = status;
+}
+
+function addBookToLibrary() {
+    // TODO
+}
+
+function render() {
+    // TODO
+}
