793138435ee4679755507d90a3773c8a88ead916
awexli <awexli@mail.sfsu.edu>
2020-01-19 21:10:14 -0800

    complete basic render book data

--- library.js
+++ library.js
@@ -1,16 +1,51 @@
-let myLibrary = [];
+const myLibrary = [['george','lucas','read']]
 
-function Book(title, author, status) {
-    // TODO
-    this.title = title;
-    this.author = author;
-    this.status = status;
+class Book {
+    constructor(title, author, status) {
+        // TODO
+        this.title = title
+        this.author = author
+        this.status = status
+    }
+
+    addBookToLibrary() {
+        const newBook = [this.title, this.author, this.status];
+        myLibrary.push(newBook)
+    }
+
+    render() {
+        renderCard(myLibrary.length-1);
+    }
 }
 
-function addBookToLibrary() {
-    // TODO
+const addBook = document.getElementById('add')
+addBook.addEventListener('click', () => {
+    const newBook = new Book('a', 'b', 'c');
+    newBook.addBookToLibrary();
+    newBook.render();
+    console.log(myLibrary)
+})
+
+function initialRender() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        renderCard(i);
+    }
 }
 
-function render() {
-    // TODO
+function renderCard(index) {
+    const newCard = document.createElement('div')
+    const renderTitle = document.createElement('P')
+    const renderAuthor = document.createElement('P')
+    const renderStatus = document.createElement('P')
+    renderTitle.innerText = myLibrary[index][0]
+    renderAuthor.innerText = myLibrary[index][1]
+    renderStatus.innerText = myLibrary[index][2]
+    newCard.appendChild(renderTitle)
+    newCard.appendChild(renderAuthor)
+    newCard.appendChild(renderStatus)
+    document.querySelector('.card').appendChild(newCard)
+}
+
+window.onload = function() {
+    initialRender();
 }
