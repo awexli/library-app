1491b2e0ce3c0e4c96577786b0990a2345d0060a
awexli <awexli@mail.sfsu.edu>
2020-01-28 18:36:35 -0800

    remove touchstart

--- library.js
+++ library.js
@@ -31,7 +31,7 @@ class Book {
 
 function listen() {
     var currCard, currId;
-    document.addEventListener('touchstart click', (e) => {
+    document.addEventListener('click', (e) => {
         const card = e.path[2];
         const bookId = parseInt(card.id);
 
